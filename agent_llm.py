90a5297a9972cb122275041a554cbddd4b9b3217
Chinmay Singh <chinmay.singh@gmail.com>
2024-07-22 20:45:22 +0000

    azure openai

--- agent_llm.py
+++ agent_llm.py
@@ -1,21 +1,31 @@
 import os
-from openai import OpenAI
-
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+from openai import AzureOpenAI
 import numpy as np
 import textworld
 
 
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+    api_version="2024-02-01",
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
 def llm(prompt, stop=["\n"]):
-    response = client.completions.create(model="gpt-3.5-turbo-instruct",
-    prompt=prompt,
-    temperature=0,
+    completion = client.chat.completions.create(
+    model="gpt-4-0125-preview",
+    messages= [
+    {
+      "role": "user",
+      "content": prompt
+    }],
     max_tokens=100,
+    temperature=0,
     top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=stop)
-    return response.choices[0].text
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=stop
+    )
+    return completion.choices[0].message.content
 
 
 class LLMAgent(textworld.Agent):
