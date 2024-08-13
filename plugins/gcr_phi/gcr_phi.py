dae0bdaca48651effe3922e40ddabfc0c55bda0f
Chinmay Singh <chsingh@microsoft.com>
2024-08-13 15:24:04 -0400

    Token counts (#9)
    * reduced context
    
    * enable wandb
    
    * use openai
    
    * track moves
    
    * wb trace
    
    * token usage
    
    * updated workflow
    
    * context length
    
    * max score
    
    * window length
    
    * token usage
    
    * updated requirements

--- plugins/gcr_phi/gcr_phi.py
+++ plugins/gcr_phi/gcr_phi.py
@@ -1,10 +1,13 @@
 import os
 import json
 import llm
+import time
 import requests
 import urllib.request
 from typing import Optional
+from transformers import LlamaTokenizerFast
 from pydantic import field_validator, Field
+from wandb.sdk.data_types.trace_tree import Trace
 
 @llm.hookimpl
 def register_models(register):
@@ -15,6 +18,8 @@ class GCRPhi(llm.Model):
     api_key = os.getenv("GCR_PHI_API_KEY")
     base_url = os.getenv("GCR_PHI_ENDPOINT")
     deployment_id = "phi-3-medium-128k-instruct-1"
+
+    tokenizer = LlamaTokenizerFast.from_pretrained("hf-internal-testing/llama-tokenizer")
 
     class Options(llm.Options):
         temperature: Optional[float] = Field(
@@ -29,7 +34,11 @@ class GCRPhi(llm.Model):
             if not 0 <= temperature <= 1:
                 raise ValueError("temperature must be between 0 and 1")
             return temperature
-        
+    
+    def count_tokens(self, text):
+        tokens = self.tokenizer.encode(text)
+        return len(tokens)
+    
     def execute(self, prompt, stream, response, conversation):
         headers = {
             "Content-Type": "application/json",
@@ -61,6 +70,8 @@ class GCRPhi(llm.Model):
         # Remove this header to have the request observe the endpoint traffic rules
         headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ self.api_key), 'azureml-model-deployment': self.deployment_id}
 
+        start_time = time.time()
+
         req = urllib.request.Request(self.base_url, body, headers)
 
         try:
@@ -69,6 +80,26 @@ class GCRPhi(llm.Model):
             print("The request failed with status code")
 
         result = json.loads(result.decode("utf-8"))["output"]
+        end_time = time.time()
+
+        token_usage = { "prompt_tokens": self.count_tokens(prompt.prompt), "completion_tokens": self.count_tokens(result) }
+        token_usage["total_tokens"] = token_usage["prompt_tokens"] + token_usage["completion_tokens"]
+
+        root_span = Trace(
+            name="agent_llm",
+            kind="llm",
+            metadata={
+                "temperature": prompt.options.temperature or 0.0,
+                "token_usage": token_usage,
+
+            },
+            start_time_ms=start_time,
+            end_time_ms=end_time,
+            inputs={"query": prompt.prompt},
+            outputs={"response": result, "token_usage": token_usage}
+        )
+
+        root_span.log(name="phi_trace")
 
         if result.startswith("<|assistant|>"):
             result = result[len("<|assistant|>"):].strip()
