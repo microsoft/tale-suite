ed9df0d1da2f7aa61905e7a7f1177babd41053b4
Chinmay Singh <chsingh@microsoft.com>
2024-08-21 15:10:50 +0000

    changed input format

--- agent_llm.py
+++ agent_llm.py
@@ -45,10 +45,11 @@ class LLMAgent(textworld.Agent):
                   '------------\nInput: {"feedback": "You are in a closet. There is a gun on the floor. Better get it. To exit, go east. \n\nYou can see a small black pistol here.", "admissible_commands": [\'north\', \'take pistol\', \'east\', \'push pistol to floor\']}\nOutput: take pistol\n------------\n'
                   '------------\nInput: {"feedback": "You are still on the streets. To the north is a restraunt where the mayor ate often. To the east is the Mayor\'s home.", "admissible_commands": [\'west\', \'east\', \'north\', \'put paper down\']}\nOutput: east\n------------\n'
         )
-        input = '------------\nInput: {"feedback": ' + f'"{game_state.feedback}",'  + '"admissible_commands":' + str(game_state.admissible_commands) + "}\nOutput: "
         if self.conversation:
+            input = game_state.feedback
             response = self.conversation.prompt(input, system=system_prompt, temperature=self.temperature, context=self.context)
         else:
+            input = '------------\nInput: {"feedback": ' + f'"{game_state.feedback}",'  + '"admissible_commands":' + str(game_state.admissible_commands) + "}\nOutput: "
             response = self.model.prompt(system_prompt + context + input, temperature=self.temperature)
         
         action = response.text()
