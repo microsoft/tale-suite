5d81e1415dc783b5e4ba21cc8724ed3e954ece19
Christopher Zhang Cui <czcui@ucsd.edu>
2025-08-30 07:15:56 +0000

    Cleaning up code in envs

--- tales/textworld/textworld_env.py
+++ tales/textworld/textworld_env.py
@@ -45,29 +45,3 @@ class TWCookingEnv(TextWorldEnv):
                 self.env = None
 
         return super().reset(seed=seed, options=options)
-    
-
-# class TWTrainCookingEnv(TextWorldEnv):
-#     # Making this a seperate class for now, not sure if this is the best way
-#     def __init__(self, difficulties = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], games_per_difficulty=5, shuffle = False, *args, **kwargs):
-#         self.gamefiles = []
-#         self.shuffle = shuffle
-#         for difficulty in difficulties:
-#             gamefiles = sorted(textworld_data.get_cooking_game(difficulty, split="train"))
-#             self.gamefiles += gamefiles[:games_per_difficulty]
-#         if self.shuffle:
-#             np.random.shuffle(self.gamefiles)
-#         super().__init__(self.gamefiles[0], *args, **kwargs)
-
-#     def reset(self, *, seed=None, options=None):
-
-#         # Switch everytime during training
-#         if self.shuffle:
-#             # Shuffle all game files and take the first one
-#             np.random.shuffle(self.gamefiles)
-#             self.gamefile = self.gamefiles[0]
-#             if self.env is not None:
-#                 self.env.close()
-#                 self.env = None
-
-#         return super().reset(seed=seed, options=options)
