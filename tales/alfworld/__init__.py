4fdd04e0c8eb399cddd09bda535c56dda401e77e
Christopher Cui <49284735+christopherzc@users.noreply.github.com>
2026-05-01 12:14:11 -0700

    Tt split (#33)
    * Added scienceworld and jericho splits, fixed alfworld split bug
    
    * Added train and test splits directly into env init
    
    * Got rid of get_env_split (not used anymore)
    
    Signed-off-by: Christopher Zhang Cui <czcui@ucsd.edu>
    
    * Update and merge Readmes. Remove Jericho unofficial train split
    
    * Apply suggestion from @MarcCote
    
    ---------
    
    Signed-off-by: Christopher Zhang Cui <czcui@ucsd.edu>
    Co-authored-by: Marc-Alexandre Côté <marc.cote.19@gmail.com>

--- tales/alfworld/__init__.py
+++ tales/alfworld/__init__.py
@@ -4,9 +4,14 @@ from .alfworld_data import TASK_TYPES, prepare_alfworld_data
 from .alfworld_env import ALFWorldTask
 
 environments = []
+train_environments = []
 
 for split in ["seen", "unseen"]:
     for task_type in TASK_TYPES:
+        gamefiles = sorted(alfworld_data.get_alfworld_game(task_type, split))
+        train_gamefiles = gamefiles[1:]
+        test_gamefiles = [gamefiles[0]]
+
         task_name = task_type.replace("_", " ").title().replace(" ", "")
         env_name = f"ALFWorld{task_name}{split.title()}"
         environments.append([env_name, "v0"])
@@ -14,7 +19,21 @@ for split in ["seen", "unseen"]:
         gym.register(
             id=f"tales/{env_name}-v0",
             entry_point="tales.alfworld:ALFWorldTask",
-            kwargs={"task_type": task_type, "split": split},
+            kwargs={
+                "all_gamefiles": test_gamefiles,
+                "start_gamefile": test_gamefiles[0],
+            },
+        )
+
+        train_env_name = env_name + "_train"
+        train_environments.append([train_env_name, "v0"])
+        gym.register(
+            id=f"tales/{train_env_name}-v0",
+            entry_point="tales.alfworld:ALFWorldTask",
+            kwargs={
+                "all_gamefiles": train_gamefiles,
+                "start_gamefile": train_gamefiles[0],
+            },
         )
 
 
--- tales/textworld/__init__.py
+++ tales/textworld/__init__.py
@@ -4,16 +4,28 @@ from .textworld_data import prepare_twcooking_data
 from .textworld_env import TextWorldEnv, TWCookingEnv
 
 environments = []
+train_environments = []
 
 # TWCookingEnv
 for difficulty in range(1, 10 + 1):
+    gamefiles = sorted(textworld_data.get_cooking_game(difficulty))
+    train_gamefiles = gamefiles[1:]
+    test_gamefiles = [gamefiles[0]]
     env_name = f"TWCookingLevel{difficulty}"
     environments.append([env_name, "v0"])
 
     gym.register(
         id=f"tales/{env_name}-v0",
         entry_point="tales.textworld:TWCookingEnv",
-        kwargs={"difficulty": difficulty},
+        kwargs={"all_gamefiles": test_gamefiles, "start_gamefile": test_gamefiles[0]},
+    )
+
+    train_env_name = env_name + "_train"
+    train_environments.append([train_env_name, "v0"])
+    gym.register(
+        id=f"tales/{train_env_name}-v0",
+        entry_point="tales.textworld:TWCookingEnv",
+        kwargs={"all_gamefiles": train_gamefiles, "start_gamefile": train_gamefiles[0]},
     )
 
 
